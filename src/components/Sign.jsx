563f89cb0d117802dcb8f50a02dc09d62aafd4e2
Melone <melone@192.168.1.27>
2024-01-12 00:27:15 +0000

    begin logic

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 export default function Sign() {
+  const [text, setText] = useState("");
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  function redirection() {
+    if (text == "") {
+      toast.error("une erreur est survenue");
+    } else {
+      navigate("/response");
+    }
+  }
+  console.log(text);
   return (
     <div className="content">
       <div className="app">
@@ -35,21 +52,26 @@ export default function Sign() {
               And much more!
             </li>
           </ul>
-          <div className="divForInput">
-            <label htmlFor="email">Email address</label>
-            <input
-              className="inputText"
-              type="text"
-              placeholder="email@company.com"
-              required
-            />
-            <input
-              onClick={() => navigate("/response")}
-              className="inputButton"
-              type="button"
-              value="Subscribe to monthly newsletter"
-            />
-          </div>
+          <form action="">
+            {" "}
+            <div className="divForInput">
+              <label htmlFor="email">Email address</label>
+              <input
+                className="inputText"
+                type="text"
+                placeholder="email@company.com"
+                required
+                value={text}
+                onChange={(e) => handleChange(e)}
+              />
+              <input
+                onClick={() => redirection()}
+                className="inputButton"
+                type="submit"
+                value="Subscribe to monthly newsletter"
+              />
+            </div>
+          </form>
         </div>
         <div className="content-right">
           <img src="/images/illustration-sign-up-desktop.svg" alt="" />
